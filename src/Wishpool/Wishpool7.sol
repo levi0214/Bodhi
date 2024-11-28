b3033614e5e830f7b727bb4b781f8076ca273ad2
Luyao <zhangluyao.levi@gmail.com>
2024-11-28 17:03:53 +0800

    initial version of wp7: drop close wish

--- src/Wishpool/Wishpool7.sol
+++ src/Wishpool/Wishpool7.sol
@@ -9,13 +9,12 @@ error Unauthorized();
 error InvalidResponse();
 error EtherTransferFailed();
 
-contract Wishpool6 is ERC1155TokenReceiver {
+contract Wishpool7 is ERC1155TokenReceiver {
     IBodhi public immutable BODHI;
 
     struct Wish {
         address creator;
-        address solver;
-        bool isOpen;
+        address solver;    // Optional designated solver
     }
 
     struct Response {
@@ -29,7 +28,7 @@ contract Wishpool6 is ERC1155TokenReceiver {
 
     event CreateWish(uint256 indexed wishId, address indexed creator, address indexed solver);
     event CreateResponse(uint256 indexed wishId, address indexed solver, uint256 responseId);
-    event CloseWish(
+    event Reward(
         uint256 indexed wishId,
         address indexed solver,
         uint256 indexed responseId,
@@ -43,14 +42,14 @@ contract Wishpool6 is ERC1155TokenReceiver {
 
     function createWish(string calldata arTxId, address solver) external {
         uint256 wishId = BODHI.assetIndex();
-        wishes[wishId] = Wish(msg.sender, solver, true);
+        wishes[wishId] = Wish(msg.sender, solver);
         emit CreateWish(wishId, msg.sender, solver);
         BODHI.create(arTxId);
     }
 
     function createResponse(uint256 wishId, string calldata arTxId) external {
         Wish memory wish = wishes[wishId];
-        if (!wish.isOpen || wish.creator == address(0)) revert InvalidWish();
+        if (wish.creator == address(0)) revert InvalidWish();
         if (wish.solver != address(0) && msg.sender != wish.solver) revert Unauthorized();
 
         uint256 responseId = BODHI.assetIndex();
@@ -60,26 +59,29 @@ contract Wishpool6 is ERC1155TokenReceiver {
         BODHI.create(arTxId);
     }
 
-    function closeWish(uint256 wishId, uint256 responseId) external {
-        Wish storage wish = wishes[wishId];
-        if (!wish.isOpen) revert InvalidWish();
+    function reward(uint256 wishId, uint256 responseId, uint256 amount) external {
+        Wish memory wish = wishes[wishId];
         if (msg.sender != wish.creator && msg.sender != wish.solver) revert Unauthorized();
 
         Response storage response = responses[responseId];
         if (response.creator == address(0) || response.wishId != wishId) revert InvalidResponse();
         if (wish.solver != address(0) && response.creator != wish.solver) revert Unauthorized();
+        if (response.isRewarded) revert InvalidResponse();
 
-        wish.isOpen = false;
         response.isRewarded = true;
 
         uint256 balance = BODHI.balanceOf(address(this), wishId);
         uint256 supply = BODHI.totalSupply(wishId);
-        uint256 amount = balance + 1 ether > supply ? supply - 1 ether : balance;
-        uint256 sellPrice = BODHI.getSellPriceAfterFee(wishId, amount);
+        uint256 rewardAmount = amount == 0 ? 
+            (balance + 1 ether > supply ? supply - 1 ether : balance) : 
+            amount;
+            
+        uint256 sellPrice = BODHI.getSellPriceAfterFee(wishId, rewardAmount);
 
-        emit CloseWish(wishId, response.creator, responseId, amount, sellPrice);
-        if (amount > 0) {
-            BODHI.sell(wishId, amount);
+        emit Reward(wishId, response.creator, responseId, rewardAmount, sellPrice);
+        
+        if (rewardAmount > 0) {
+            BODHI.sell(wishId, rewardAmount);
             (bool sent,) = response.creator.call{value: sellPrice}("");
             if (!sent) revert EtherTransferFailed();
         }
